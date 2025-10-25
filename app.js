952ca949ab2781e57aee6f6e63f6b7cf3f91c6cd
Kojowest <kojowest2012@gmail.com>
2025-10-25 02:19:22 +0000

    learning about the javascript operation system

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 const os = require("os");
 
 //info about the current user
+const user = os.userInfo();
+console.log(user);
+
+//method returns the system uptime
+console.log(`The system uptime is ${os.uptime()} seconds`);
+
+const currentOs = {
+  name: os.type(),
+  release: os.release(),
+  totalmem: os.totalmem(),
+  freemem: os.freemem(),
+};
+
+console.log(currentOs);
